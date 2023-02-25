626e59a388e6e07b4e2c0ee316e5e4f0d3e16a47
codestronomer <johnrumide6@gmail.com>
2023-02-25 02:13:53 +0100

    [fix] restructure code

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -3,6 +3,8 @@
 Module implements a FIFO caching system that
 inherits from BaseCaching
 """
+from collections import OrderedDict
+
 BaseCaching = __import__('base_caching').BaseCaching
 
 
@@ -15,6 +17,7 @@ class FIFOCache(BaseCaching):
         Initializes class Instance
         """
         super().__init__()
+        self.cache_data = OrderedDict()
 
     def put(self, key, item):
         """
@@ -25,8 +28,8 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
         if len(self.cache_data) > BaseCaching.MAX_ITEMS:
             del_key = list(self.cache_data)[0]
-            print("DISCARD: {}".format(del_key[0]))
-            del self.cache_data[del_key]
+            self.cache_data.pop(del_key)
+            print("DISCARD: {}".format(del_key))
 
     def get(self, key):
         """
