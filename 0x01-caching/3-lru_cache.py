2195bad0cb753c70d3b24d0797ebf4144787b4f9
codestronomer <johnrumide6@gmail.com>
2023-02-25 02:46:16 +0100

    [fix] rewrite 3-lru_cache.py

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -1,39 +1,42 @@
 #!/usr/bin/env python3
-"""Least Recently Used caching module.
+"""
+Module implements a LRU caching system that
+inherits from BaseCaching
 """
 from collections import OrderedDict
 
-from base_caching import BaseCaching
+BaseCaching = __import__('base_caching').BaseCaching
 
 
 class LRUCache(BaseCaching):
-    """Represents an object that allows storing and
-    retrieving items from a dictionary with a LRU
-    removal mechanism when the limit is reached.
+    """
+    objects implements a LRU cache replacement policy
+    in a simple caching system
     """
     def __init__(self):
-        """Initializes the cache.
+        """
+        Initializes class Instance
         """
         super().__init__()
         self.cache_data = OrderedDict()
 
     def put(self, key, item):
-        """Adds an item in the cache.
         """
-        if key is None or item is None:
+        adds an item to the cache using the FIFO algorithm
+        """
+        if item is None or key is None:
             return
-        if key not in self.cache_data:
-            if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                lru_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", lru_key)
-            self.cache_data[key] = item
-            self.cache_data.move_to_end(key, last=False)
-        else:
-            self.cache_data[key] = item
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            del_key = list(self.cache_data)[0]
+            self.cache_data.pop(del_key)
+            print("DISCARD: {}".format(del_key))
 
     def get(self, key):
-        """Retrieves an item by key.
         """
-        if key is not None and key in self.cache_data:
-            self.cache_data.move_to_end(key, last=False)
+        retrieves an item from self.cache_data with key
+        and moves the item to the top of the queue
+        """
+        if key in self.cache_data:
+            self.cache_data.move_to_end(key)
         return self.cache_data.get(key, None)
