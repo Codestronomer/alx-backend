888ff275163b81c745641ca568c013b9ef9f39d3
codestronomer <johnrumide6@gmail.com>
2023-02-25 01:29:55 +0100

    [fix] error handling

--- 0x01-caching/0-basic_cache1.py
+++ 0x01-caching/0-basic_cache1.py
@@ -8,19 +8,24 @@ BaseCaching = __import__('base_caching').BaseCaching
 
 class BasicCache(BaseCaching):
     """
-    An Object that implements a Basic Caching System
-    for storing and retrieving items
+    A Basic Caching System
     """
+    def __init__(self):
+        """Initialize class Instance"""
+        self.cache_data = BaseCaching.cache_data
+
     def put(self, key, item):
         """
         Inserts an item into the cache
         """
-        if item is None or key is None:
-            return
-        setattr(self.cache_data, key, item)
+        if item is not None && key is not None:
+            setattr(self.cache_data, key, item)
 
     def get(self, key):
         """
         Gets an item from the cache if key is None
         """
-        return self.cache_data.get(key, None)
+        if key is not None:
+            if key in self.cache_data:
+                return self.cache_data.get(key)
+        return None
