70dcbebc4f81ec3d510d444775e2aa4c071c0961
codestronomer <johnrumide6@gmail.com>
2023-03-01 03:59:43 +0100

    [fix] 0-app.py, [add] 1-app.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,13 +5,18 @@ and localization in flask
 """
 from flask import Flask, render_template
 
+
 # initialize flask app
 app = Flask(__name__)
 
-# Home route ("/")
-@app.route('/')
-def hello_world():
+
+@app.route('/', strict_slashes=False)
+def home():
     """
     defines a flask route that returns a template
     """
-    return render_template('index.html')
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host="0.0.0.0", debug=True)
---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+"""
+Module contains a simple flask app to demonstrate internalization
+and localization in flask
+"""
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config(object):
+    """
+    object defines babel configuration for Flask application
+    """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+# initialize flask app
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def home() -> str:
+    """
+    Flask application entry point
+    """
+    return render_template('1-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port=5000, host="0.0.0.0", debug=True)
