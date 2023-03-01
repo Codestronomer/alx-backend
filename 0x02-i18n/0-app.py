af8ae77ec52cd60a9cade915b21abb67840e108e
John Rumide <johnrumide6@gmail.com>
2023-03-01 05:15:13 +0100

    Add files via upload

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,22 +1,20 @@
 #!/usr/bin/env python3
 """
-Module contains a simple flask app to demonstrate internalization
-and localization in flask
+Flask app
 """
 from flask import Flask, render_template
 
 
-# initialize flask app
 app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def home() -> str:
+def index() -> str:
     """
-    defines a flask route that returns a template
+    Handles / route
     """
     return render_template('0-index.html')
 
 
-if __name__ == '__main__':
-    app.run(port=5000, host="0.0.0.0", debug=True)
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
